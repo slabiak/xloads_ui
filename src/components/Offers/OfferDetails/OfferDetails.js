b7d3428997c6f1889356a5f43caf5f64c0f18d94
Tomasz <slabiak.tomasz@gmail.com>
2020-08-14 16:04:05 +0200

    add layout to OfferDetail view

--- src/components/Offers/OfferDetails/OfferDetails.js
+++ src/components/Offers/OfferDetails/OfferDetails.js
@@ -1,15 +1,75 @@
 import React from "react";
 import { connect } from "react-redux";
+import classes from "./OfferDetails.module.css";
+import Header from "../../Header/Header";
+import MapComp from "../../MapComp/MapComp";
+import Settings from "../../Settings/Settings";
+import Search from "../../Search/Search";
+import OffersHeader from "../../Offers/OffersHeader/OffersHeader";
 
 function OfferDetails(props) {
+  const [currentView, setCurrentView] = React.useState("list");
+  const onChangeViewHandler = (e, newView) => {
+    e.preventDefault();
+    setCurrentView(newView);
+  };
+
   let offer = props.offers.filter(
     (offer) => offer.id == props.match.params.id
   )[0];
+  let header = currentView === "list" ? <Header /> : null;
+  let offerPresent = null;
   if (offer != null) {
-    return <p>{offer.title}</p>;
+    let offersHeader = (
+      <OffersHeader
+        numberOfOffers={props.numberOfOffers}
+        currentView={props.currentView}
+        onChangeViewHandler={onChangeViewHandler}
+      />
+    );
+
+    let search = currentView === "list" ? <Search></Search> : null;
+    let map = (
+      <MapComp view={currentView} hooveredOffer={props.hooveredOffer}></MapComp>
+    );
+
+    let offerDetail = currentView === "list" ? <p>{offer.title}</p> : null;
+
+    let homePage = (
+      <React.Fragment>
+        {search}
+        {map}
+      </React.Fragment>
+    );
+
+    offerPresent = (
+      <div
+        className={
+          currentView === "list" ? classes.Container : classes.ContainerMap
+        }
+      >
+        {header}
+
+        <div className={classes.OfferDetail}>
+          {offersHeader}
+          {offerDetail}
+        </div>
+        {homePage}
+      </div>
+    );
   } else {
-    return "Brak danych";
+    offerPresent = (
+      <div
+        className={
+          currentView === "list" ? classes.Container : classes.ContainerMap
+        }
+      >
+        {header}
+      </div>
+    );
   }
+
+  return offerPresent;
 }
 
 const mapStateToProps = (state) => {
